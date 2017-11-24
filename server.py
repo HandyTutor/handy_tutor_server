3d2ae388767d1c3feffa828cb99ada12f195022c
Ubuntu <ubuntu@ip-172-31-14-221.ap-northeast-2.compute.internal>
2017-11-24 17:21:44 +0900

    getVideoList added

--- server.py
+++ server.py
@@ -1,9 +1,21 @@
+#-*- coding: utf-8 -*-
+
 from flask import Flask
+import pymysql
+import json
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-	return 'hello world'
+conn = pymysql.connect(host = 'localhost', user = 'root', password = 'digh3484', db = 'handy_tutor', charset = 'utf8')
+
+
+@app.route('/video_list')
+def getVideoList():
+	cursor = conn.cursor(pymysql.cursors.DictCursor)
+	cursor.execute('SELECT * FROM VIDEO');
+	rows = cursor.fetchall()
+	return json.dumps(rows)
+
 
 if __name__ == '__main__':
 	app.run(host = '0.0.0.0', port=5000, debug=True)
