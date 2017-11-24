ce7bad8d13e56eb80747ba2c91c1a00a7a561e9a
Ubuntu <ubuntu@ip-172-31-14-221.ap-northeast-2.compute.internal>
2017-11-24 18:03:36 +0900

    getVideo added

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-#-*- coding: utf-8 -*-
+#-- coding: utf-8 --
 
 from flask import Flask
+from flask import request
 import pymysql
 import json
 
@@ -14,8 +15,16 @@ def getVideoList():
 	cursor = conn.cursor(pymysql.cursors.DictCursor)
 	cursor.execute('SELECT * FROM VIDEO');
 	rows = cursor.fetchall()
-	return json.dumps(rows)
 
+	return json.dumps(rows, encoding = 'utf-8')
+
+@app.route('/video', methods = ['POST'])
+def getVideo():
+
+	cursor = conn.cursor(pymysql.cursors.DictCursor)
+	cursor.execute('SELECT KOREAN, ENGLISH, ROLE, TIME FROM SUBTITLE WHERE VIDEO_KEY = "' + request.form['video_key'] + '"');
+	rows = cursor.fetchall()
+	return json.dumps(rows, encoding = 'utf-8')
 
 if __name__ == '__main__':
 	app.run(host = '0.0.0.0', port=5000, debug=True)
