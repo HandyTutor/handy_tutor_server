78418d18bc1846c09c4d99e6f4efc22ff401f5bf
HongSH1213 <seokh1213@gmail.com>
2017-12-08 12:41:16 +0900

    Update server.py

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask import request
 import pymysql
 import json
+from PhraseSimilarity import SimilarityScore
 
 app = Flask(__name__)
 
@@ -16,7 +17,7 @@ def getVideoList():
 	cursor.execute('SELECT * FROM VIDEO');
 	rows = cursor.fetchall()
 
-	return json.dumps(rows, encoding = 'utf-8')
+	return json.dumps(rows)
 
 @app.route('/video', methods = ['POST'])
 def getVideo():
@@ -24,7 +25,12 @@ def getVideo():
 	cursor = conn.cursor(pymysql.cursors.DictCursor)
 	cursor.execute('SELECT KOREAN, ENGLISH, ROLE, TIME FROM SUBTITLE WHERE VIDEO_KEY = "' + request.form['video_key'] + '" ORDER BY TIME ASC');
 	rows = cursor.fetchall()
-	return json.dumps(rows, encoding = 'utf-8')
+	return json.dumps(rows)
+
+@app.route('/phrase_similarity', methods = ['POST'])
+def getSimilarity():
+	return str(SimilarityScore(request.form['input1'], request.form['input2']))
+	
 
 if __name__ == '__main__':
-	app.run(host = '0.0.0.0', port=5000, debug=True)
+	app.run(host = '0.0.0.0', port=5002, debug=True)
